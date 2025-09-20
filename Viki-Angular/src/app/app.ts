9e5884f92e211726b419ef69ed490065179f757e
danielyordanov07 <98211151+danielyordanov07@users.noreply.github.com>
2025-09-20 23:01:26 +0300

    meh

--- Viki-Angular/src/app/app.ts
+++ Viki-Angular/src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Header
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.sass'
+  styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('Viki-Angular');
